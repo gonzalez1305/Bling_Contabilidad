4af3aec4477eda7531e3a174e7e9d64e9806c644
Ramo-04 <juanandresramirezmunoz94@gmail.com>
2024-06-27 01:24:25 -0500

    Prueba

--- indexCorreo.php
+++ indexCorreo.php
@@ -8,8 +8,8 @@
         body {
             font-family: Arial, sans-serif;
             background-color: #f4f4f9;
-            margin: 0;
-            padding: 0;
+            margin: 15;
+            padding: 10;
             display: flex;
             justify-content: center;
             align-items: center;
