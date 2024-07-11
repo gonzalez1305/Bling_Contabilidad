3730347abc3f93b2946dffdacc65a21df1525109
Ramo-04 <152299855+Ramo-04@users.noreply.github.com>
2024-07-11 14:15:27 -0500

    Update elim_venta.php

--- elim_venta.php
+++ elim_venta.php
@@ -1,28 +1,28 @@
 <?php
 // Conectar a la base de datos
-$conexion = mysqli_connect('localhost:3307', 'root', '', 'bling_o');
+$conectar = mysqli_connect('localhost:3307', 'root', '', 'bling_o');
 
 // Definir la variable $id_venta
 $id_venta = $_GET['id_venta'];
 
-if (!$conexion) {
+if (!$conectar) {
     echo "Error: No se pudo conectar a la base de datos.";
     exit;
 }
 
 // Eliminar las transacciones asociadas a la venta
 $sql = "DELETE FROM transacciones WHERE fk_id_pago = $id_venta";
-$resultado = mysqli_query($conexion, $sql);
+$resultado = mysqli_query($conectar, $sql);
 
 if ($resultado) {
     // Eliminar el pago
     $sql = "DELETE FROM pago WHERE fk_id_venta = $id_venta";
-    $resultado = mysqli_query($conexion, $sql);
+    $resultado = mysqli_query($conectar, $sql);
 
     if ($resultado) {
         // Eliminar la venta
         $sql = "DELETE FROM venta WHERE id_venta = $id_venta";
-        $resultado = mysqli_query($conexion, $sql);
+        $resultado = mysqli_query($conectar, $sql);
 
         if ($resultado) {
             header('Location: ventas_list.php');
